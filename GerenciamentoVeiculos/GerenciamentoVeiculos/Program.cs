aa813d6b326e998bc1bf5bb5ee99c48270952eb5
SartoriGuilherme <128654718+SartoriGuilherme@users.noreply.github.com>
2024-08-08 21:47:13 -0300

    Add files via upload

---
+++ GerenciamentoVeiculos/GerenciamentoVeiculos/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoVeiculos
+{
+    public class Veiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        public Veiculo(string marca, string modelo)
+        {
+            this.Marca = marca;
+            this.Modelo = modelo;
+        }
+
+        public virtual void ExibirDetalhes()
+        {
+            Console.WriteLine($"Marca: {Marca}, Modelo: {Modelo}");
+        }
+    }
+
+    public class Carro : Veiculo
+    {
+        public int NumerodePortas { get; set; }
+
+        public Carro(string marca, string modelo, int numerodePortas) : base(marca, modelo)
+        {
+            this.NumerodePortas = numerodePortas;
+        }
+
+        public override void ExibirDetalhes()
+        {
+            base.ExibirDetalhes();
+            Console.WriteLine($"Numero de portas é {NumerodePortas}");
+        }
+    }
+
+    public class Moto : Veiculo
+    {
+        public int Cilindrada { get; set; }
+
+        public Moto(string marca, string modelo, int cilindrada) : base(marca, modelo)
+        {
+            this.Cilindrada = cilindrada;
+        }
+
+        public override void ExibirDetalhes()
+        {
+            base.ExibirDetalhes();
+            Console.WriteLine($"É uma moto, Cilindrada {Cilindrada}");
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Veiculo meuCarro = new Carro("Fiat", "Palio Weekend", 0);
+                Veiculo minhaMoto = new Moto("Honda", "CG", 125);
+
+                meuCarro.ExibirDetalhes();
+                minhaMoto.ExibirDetalhes();
+            }
+        }
+    }
+}
