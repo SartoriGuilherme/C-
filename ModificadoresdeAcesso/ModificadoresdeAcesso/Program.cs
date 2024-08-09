ec66cb34138a747d09a8247d79f3fdcc44f5e0c4
SartoriGuilherme <128654718+SartoriGuilherme@users.noreply.github.com>
2024-08-08 21:46:44 -0300

    Add files via upload

---
+++ ModificadoresdeAcesso/ModificadoresdeAcesso/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadoresdeAcesso
+{
+
+    public class Pessoa
+    {
+        private string nome;
+        private int idade;
+
+        public Pessoa(string nome = "Nome não informado", int idade = 10)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                if (nome != null)
+                {
+                    nome = value;
+                }
+            }
+        }
+
+        public int Idade
+        {
+            get { return idade; }
+            set
+            {
+                if (value > 0 && value < 140)
+                {
+                    idade = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Idade não pode ser negativa");
+                }
+            }
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Pessoa pessoa = new Pessoa("Raimundo", -1);
+                { Console.WriteLine(pessoa.nome); }
+                { Console.WriteLine(pessoa.idade); }
+                //               try
+                //               {
+                //                   pessoa.Nome = "Raimundo";
+                //                   pessoa.Idade = 100;
+                //               }
+                //               catch (Exception ex)
+                //              {
+                //                  Console.WriteLine(ex.Message);
+                //               }
+            }
+        }
+    }
+}
+    
---
+++ Produtos/Construtores/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Construtores
+{
+    public class Produto 
+    {
+        private string nome { get; set; }
+        private float preco { get; set; }
+        private float quantidade { get; set; }
+
+        public Produto(string nome, float preco, float quantidade)
+        {
+        this.Nome = nome;
+        this.Preco = preco;
+        this.Quantidade = quantidade;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                if (nome != null)
+                {
+                    nome = value;
+                }
+            }
+        }
+
+        public float Preco
+        {
+            get { return preco; }
+            set
+            {
+                if (value > 0)
+                {
+                    preco = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Preco não pode ser negativo ou zero");
+                }
+            }
+        }
+
+        public float Quantidade
+        {
+            get { return quantidade; }
+            set
+            {
+                if (value > 0)
+                {
+                    quantidade = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Quantidade não pode ser negativa ou zero");
+                }
+            }
+        }
+
+        public virtual float CalcularValorTotal()
+        {
+            return Preco * Quantidade;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+                Produto produto = new Produto("Cadeira", 0, 29);
+                { Console.WriteLine($"O produto {produto.Nome} custa {produto.CalcularValorTotal()}"); }
+            }
+            catch (ArgumentException ex)
+            { Console.WriteLine("Utilize valores maiores que 0!" ); }
+        }
+    }
+}
